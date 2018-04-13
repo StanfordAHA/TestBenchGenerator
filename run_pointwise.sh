d82abaafca08c7609580402dfc4ad4ede46bfa98
Lenny Truong <lenny@stanford.edu>
2018-04-13 23:21:37 +0000

    Use master myconvert.csh

--- run_pointwise.sh
+++ run_pointwise.sh
@@ -6,9 +6,8 @@ python wrap_cgra.py --pnr-io-collateral pointwise.io.json --cgra-verilog top.v
 python generate_harness.py --pnr-io-collateral pointwise.io.json --bitstream pointwise.bs --max-clock-cycles 5000000
 
 # png to raw
-./myconvert.csh inputs/pointwise.png io16in_in_arg_1_0_0.raw
-./myconvert.csh gold_outputs/pointwise.png gold_output.raw
-
+../CGRAFlow/CGRAGenerator/verilator/generator_z_tb/io/myconvert.csh inputs/pointwise.png io16in_in_arg_1_0_0.raw
+../CGRAFlow/CGRAGenerator/verilator/generator_z_tb/io/myconvert.csh gold_outputs/pointwise.png gold_output.raw
 VERILATOR_TOP=../CGRAFlow/CGRAGenerator/verilator/generator_z_tb/
 
 
@@ -22,3 +21,4 @@ make --silent -C obj_dir -j -f VCGRA_wrapper.mk VCGRA_wrapper -B
 ./obj_dir/VCGRA_wrapper
 
 cmp io16_out_0_0.raw gold_output.raw
+echo "SUCCESS!"
