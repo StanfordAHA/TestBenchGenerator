ab8b2f39162cd95bcc1dece3be7c4e79b0e1fb21
Lenny Truong <lenny@stanford.edu>
2018-04-16 17:31:32 +0000

    Update delay logic

--- rename_input.py
+++ rename_input.py
@@ -6,7 +6,7 @@ with open(sys.argv[1], "r") as file:
     info = json.load(file)
 
 input_file_name = sys.argv[2]
-delay_in, delay_out = sys.argv[3].split(",")
+delay_in, delay_out = [int(x) for x in sys.argv[3].split(",")]
 
 name = None
 for key in info:
@@ -20,9 +20,9 @@ assert name is not None, "We need at least one in"
 if delay_out:
     with open(input_file_name, "rb") as input_file:
         with open(f"{name}.raw", "wb") as output_file:
-            for i in range(int(delay_out)):
-                output_file.write("\0")
-            output_file.write(input_file)
+            for i in range(delay_out):
+                output_file.write(0)
+            output_file.write(input_file.read())
 else:
     shutil.copy(input_file_name, f"{name}.raw")
 
--- rename_output.py
+++ rename_output.py
@@ -7,7 +7,7 @@ with open(sys.argv[1], "r") as file:
 
 target_file_name = sys.argv[2]
 app = sys.argv[3]
-delay_in, delay_out = sys.argv[4].split(",")
+delay_in, delay_out = [int(x) for x in sys.argv[4].split(",")]
 
 name = None
 for key in info:
@@ -18,12 +18,20 @@ for key in info:
 assert name is not None, "We need at least one output"
 
 if delay_in:
-    with open(f"{name}.raw", "r+wb") as output_file:
-        output_file.write(output_file[int(delay_in):])
+    print(delay_in)
+    with open(f"{name}.raw", "rb") as output_file:
+        output_file.seek(delay_in)
+        output_data = output_file.read()
+    with open(f"{name}.raw", "wb") as output_file:
+        output_file.write(output_data)
 
 # TODO: Why do we we do this post processing for these apps?
-if app in ["conv_1_2"]:
+if app == "conv_1_2":
     import delegator
-    delegator.run(f"../CGRAGenerator/verilator/generator_z_tb/bin/{app}_convert < {name}.raw > {target_file_name}")
+    print(f"../CGRAGenerator/verilator/generator_z_tb/bin/conv_1_2_convert < {name}.raw > {target_file_name}")
+    print(delegator.run(f"../CGRAGenerator/verilator/generator_z_tb/bin/conv_1_2_convert < {name}.raw > {target_file_name}").return_code)
+elif app == "conv_bw":
+    import delegator
+    delegator.run(f"../CGRAGenerator/verilator/generator_z_tb/bin/crop31 < {name}.raw > {target_file_name}")
 else:
     shutil.copy(f"{name}.raw", target_file_name)
