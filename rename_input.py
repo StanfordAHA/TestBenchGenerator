80e321813ae5228ecee6719e4921eb297e8748a2
Lenny Truong <lenny@stanford.edu>
2018-04-16 04:11:03 +0000

    Add pre/post processing hooks

--- rename_input.py
+++ rename_input.py
@@ -6,6 +6,7 @@ with open(sys.argv[1], "r") as file:
     info = json.load(file)
 
 input_file_name = sys.argv[2]
+delay_in, delay_out = sys.argv[3].split(",")
 
 name = None
 for key in info:
@@ -15,4 +16,13 @@ for key in info:
 
 assert name is not None, "We need at least one in"
 
-shutil.copy(input_file_name, f"{name}.raw")
+
+if delay_out:
+    with open(input_file_name, "rb") as input_file:
+        with open(f"{name}.raw", "wb") as output_file:
+            for i in range(delay_out):
+                output_file.write("\0")
+            output_file.write(input_file)
+else:
+    shutil.copy(input_file_name, f"{name}.raw")
+
--- rename_output.py
+++ rename_output.py
@@ -6,6 +6,8 @@ with open(sys.argv[1], "r") as file:
     info = json.load(file)
 
 target_file_name = sys.argv[2]
+app = sys.argv[3]
+delay_in, delay_out = sys.argv[4].split(",")
 
 name = None
 for key in info:
@@ -15,4 +17,13 @@ for key in info:
 
 assert name is not None, "We need at least one output"
 
-shutil.copy(f"{name}.raw", target_file_name)
+if delay_in:
+    with open(f"{name}.raw", "r+wb") as output_file:
+        output_file.write(output_file[delay_in:])
+
+# TODO: Why do we we do this post processing for these apps?
+if app in ["conv_1_2"]:
+    import delegator
+    delegator.run(f"../CGRAGenerator/verilator/generator_z_tb/bin/{app}_convert < {name}.raw > {target_file_name}")
+else:
+    shutil.copy(f"{name}.raw", target_file_name)
